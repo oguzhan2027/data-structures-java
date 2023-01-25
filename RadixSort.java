be4ebb0bb64cac41c264b16cd269700a9ab546ab
Oğuzhan Çelik <65087063+oguzhan2027@users.noreply.github.com>
2023-01-25 11:32:38 +0300

    Radix Sort

---
+++ RadixSort.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public static void radixSort(int[] arr) {
+        int max = Arrays.stream(arr).max().getAsInt();
+
+        // Do counting sort for every digit. Note that instead
+        // of passing digit number, exp is passed. exp is 10^i
+        // where i is current digit number
+        for (int exp = 1; max / exp > 0; exp *= 10)
+            countingSort(arr, exp);
+    }
+
+    // A function to do counting sort of arr[] according to
+    // the digit represented by exp.
+    static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n]; // output array
+        int[] count = new int[10];
+
+        // Initialize count array
+        Arrays.fill(count, 0);
+
+        // Store count of occurrences in count[]
+        for (int i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+
+        // Change count[i] so that count[i] now contains
+        // actual position of this digit in output[]
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        // Build the output array
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to curent digit
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        radixSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
